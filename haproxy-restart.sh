95dfd9bc50e92687a3ddd3143b4c2dafc8dbaaab
Mathias Bogaert <mathias.bogaert@gmail.com>
2015-05-12 00:14:18 +0100

    Support port 80 too.

--- haproxy-restart.sh
+++ haproxy-restart.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -e
 
+iptables -I INPUT -p tcp --dport 80 --syn -j DROP
 iptables -I INPUT -p tcp --dport 443 --syn -j DROP
 
 sleep 1
 supervisorctl restart haproxy-ddos
 
+iptables -D INPUT -p tcp --dport 80 --syn -j DROP
 iptables -D INPUT -p tcp --dport 443 --syn -j DROP
