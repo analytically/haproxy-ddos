cea3a6d6fd6dc044e737be475da1bf8cc433e15c
Mathias Bogaert <mathias.bogaert@gmail.com>
2015-05-21 23:25:24 +0100

    Use multiport.

--- haproxy-restart.sh
+++ haproxy-restart.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 set -e
 
-iptables -I INPUT -p tcp --dport 80 --syn -j DROP
-iptables -I INPUT -p tcp --dport 443 --syn -j DROP
+iptables -I INPUT -p tcp -m multiport --dports 80,443 --syn -j DROP
 
-sleep 1
+sleep 0.5
 supervisorctl restart haproxy-ddos
 
-iptables -D INPUT -p tcp --dport 80 --syn -j DROP
-iptables -D INPUT -p tcp --dport 443 --syn -j DROP
+iptables -D INPUT -p tcp -m multiport --dports 80,443 --syn -j DROP
